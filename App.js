e37adde7a60d37366b5ea18fa91113c498cbc2e8
FFG Product <139473386+FFGProduct@users.noreply.github.com>
2024-08-21 12:16:34 +1000

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+/*import React, { useEffect } from 'react';
 import {
     LiveboardEmbed,
     AuthType,
@@ -30,7 +30,31 @@ const App = () => {
         <div style={{ width: '100%', height: '100vh' }}>
             <div id="ts-embed"></div>
         </div>
-    );
+    );*/
+
+import { init, AppEmbed, AuthType } from "@thoughtspot/visual-embed-sdk"; import React, { useEffect } from "react";
+
+init({
+  thoughtSpotHost: "https://team3.thoughtspot.cloud/#/pinboard/b6bb40a3-57dc-4b46-bfdc-9d76fbb74491",
+  authType: AuthType.Basic,
+  username: "ash@fiftyfivegroup.com.au",
+  password: "DX3w#fC6*mdTtm!eGswg",
+});
+
+export default function App() {
+  useEffect(() => {
+    const embed = new AppEmbed("#embed", {
+      modularHomeExperience: true,
+    }).render();
+  }, []);
+
+  return (
+    <div id="body">
+      <div id="embed"></div>
+    </div>
+  );
+}
+
 };
 
 export default App;
